e92f869cc47b2444f334a7192cbc3afbb8af16fa
Barnaricsi <barnar@kkszki.hu>
2026-09-23 09:09:16 +0200

    Eredmeny.cs added

---
+++ sportolo13_b/Models/Eredmeny.cs
@@ -0,0 +1,12 @@
+﻿namespace sportolo13_b.Models
+{
+    public class Eredmeny
+    {
+        public int Id { get; set; }
+        public string Competition { get; set; }
+        public string Description { get; set; }
+        public DateTime ResultTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public int SportoloId { get; set; }
+    }
+}
--- sportolo13_b/Program.cs
+++ sportolo13_b/Program.cs
@@ -1,4 +1,6 @@
 
+using Scalar.AspNetCore;
+
 namespace sportolo13_b
 {
     public class Program
@@ -19,6 +21,7 @@ namespace sportolo13_b
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference();
             }
 
             app.UseHttpsRedirection();
