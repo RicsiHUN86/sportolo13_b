dbd211bc93633962ef77e5c3d5a66c881c4a8368
Barnaricsi <barnar@kkszki.hu>
2026-09-23 09:18:21 +0200

    GetAll added

--- sportolo13_b/Controllers/EredmenyController.cs
+++ sportolo13_b/Controllers/EredmenyController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using sportolo13_b.Models;
 
 namespace sportolo13_b.Controllers
 {
-    public class EredmenyController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EredmenyController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly string ConnectionString = "server=localhost;uid=root;password=;database=sportolo13b";
+
+        [HttpGet]
+        public List<Eredmeny> GetAll()
         {
-            return View();
+            List<Eredmeny> eredmenyek = new();
+            var connector = new MySqlConnection(ConnectionString);
+            connector.Open();
+            string sql = "SELECT * FROM eredmeny";
+
+            var cmd = new MySqlCommand(sql, connector);
+            var dataReader = cmd.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                var eredmeny = new Eredmeny
+                {
+                    Id = dataReader.GetInt32(0),
+                    Competition = dataReader.GetString(1),
+                    Description = dataReader.GetString(2),
+                    ResultTime = dataReader.GetDateTime(3),
+                    UpdateTime = dataReader.GetDateTime(4),
+                    SportoloId = dataReader.GetInt32(5)
+                };
+                eredmenyek.Add(eredmeny);
+            }
+
+            connector.Close();
+            return eredmenyek;
         }
     }
 }
