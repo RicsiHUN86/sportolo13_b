34e8e0f49858367223fb435b6db28da0ad02df2c
Barnaricsi <barnar@kkszki.hu>
2026-09-23 08:52:06 +0200

    Add project files.

---
+++ sportolo13_b/Controllers/EredmenyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sportolo13_b.Controllers
+{
+    public class EredmenyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
